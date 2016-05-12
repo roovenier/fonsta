ee122f4a7f43e3bc8cbac707efa6efb9bfe9f742
Alex <fastex93@gmail.com>
2016-05-12 17:45:08 +0500

    added show module

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var install = require('./lib/install');
+var show = require('./lib/show');
 
-var actionName = process.argv.slice(2)[0];
+var commandName = process.argv.slice(2)[0];
 var fontNameArg = process.argv.slice(3)[0];
 var fontNameRaw = fontNameArg.split(':')[0];
 var fontTypes = (fontNameArg.indexOf(':') > -1) ? fontNameArg.split(':').pop().split(',') : ['regular'];
 
-if((actionName === 'install' || actionName === 'i') && fontNameRaw) {
+if((commandName === 'install' || commandName === 'i') && fontNameRaw) {
 	install(fontNameRaw, fontTypes);
+} else if ((commandName === 'show' || commandName === 's') && fontNameRaw) {
+	show(fontNameRaw);
 } else {
 	console.log('Unknown command given!');
 }
